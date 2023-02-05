678b0f46529f915a5c19d35b1fd98c9864e76fe1
mpope124 <mpope124@gmail.com>
2023-02-05 08:55:33 -0800

    Updated CSS and added an area to exort music data as well and make some style changes

--- main.js
+++ main.js
@@ -20,7 +20,6 @@ let Music = function(pMusic, pGenre){
     document.getElementById("musics").value = " ";
     document.getElementById("genre").value = " ";
     document.getElementById("musicNotes").innerHTML = "Music has been Stored: Add Another One?";
-
     displays.style.display = "block"; // Opens up the section of the subgenre
  } // Gets the value of music and the genre, pushes it into the musicArr and clears the textboxes
 
@@ -34,3 +33,27 @@ let Music = function(pMusic, pGenre){
     document.getElementById("end").innerHTML = "Thank you for Listing Music";
     console.log(subGenre)
  } // Gets the value of the sub genre and author, and pushes it into the subGenre array and clears the textboxes.
+ 
+ document.getElementById('listMusic').addEventListener('click',displayMusic);
+
+ function displayMusic() {
+      document.getElementById('musicList').innerText = '';
+      for (i = 0; i < musicArr.length; i++) {
+          let list = document.createElement('li');
+          list.innerText = 'You entered in the song ' + musicArr[i].music + ' and the genre is ' + musicArr[i].genre
+          document.getElementById('musicList').append(list);
+      }
+      
+  }
+
+  document.getElementById('listSubGenre').addEventListener('click',displaySubGenre)
+
+  function displaySubGenre() {
+   document.getElementById('subGenreList').innerText = '';
+   for (i = 0; i < subGenre.length; i++) {
+       let list = document.createElement('li');
+       list.innerText = 'You entered in the genre ' + subGenre[i].sub + ' and the artist/artists are ' + subGenre[i].authors
+       document.getElementById('subGenreList').append(list);
+   }
+   
+}
